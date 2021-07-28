aba360191fa0f16607e767435e75ec1e17f8f1b2
minjeongPark41 <pmjabcd@gmail.com>
2021-07-28 23:31:21 +0900

    Create 232.py

---
+++ 232.py
@@ -0,0 +1,36 @@
+class MyQueue {
+
+    Stack<Integer> in = new Stack<>();
+    Stack<Integer> out = new Stack<>();
+    
+    /** Initialize your data structure here. */
+    public MyQueue() {
+        
+    }
+    
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        in.push(x);
+    }
+    
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        peek();
+        return out.pop();
+    }
+    
+    /** Get the front element. */
+    public int peek() {
+        if(out.isEmpty()){
+            while(!in.isEmpty()){
+                out.push(in.pop());
+            }
+        }
+        return out.peek();
+    }
+    
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        return in.isEmpty()&&out.isEmpty();
+    }
+}
